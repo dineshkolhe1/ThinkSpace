58a6cc1a8a179c9ad23b366008575bcfc614e78a
dineshkolhe1 <dineshkolhe667@gmail.com>
2026-02-12 20:17:28 +0530

    update production fun

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,12 +27,14 @@ app.use(rateLimiter);
 
 app.use("/api/notes", nodeRoutes);
 
-if (process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../frontend/dist");
 
-    app.get((req,res) => {
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-})
+  app.use(express.static(frontendPath));
+
+  app.use((req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
 }
 
 connectDB().then(()=>{
